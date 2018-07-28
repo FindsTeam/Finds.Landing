bd736fd1a7a29b3d5c2cd0755fa76c447f5c318e
Artem Dadychenko <a.dadychenko@mail.freebee.by>
2018-07-28 19:52:01 +0300

    Set min zoom level

--- js/map.js
+++ js/map.js
@@ -5,7 +5,11 @@ function initMap() {
   var osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
   var osmAttributes = 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
 
-  var map = L.map(containerId).setView(location, zoom);
+  var map = L
+  .map(containerId, {
+    minZoom: 10,
+  })
+  .setView(location, zoom);
   L.tileLayer(osmUrl, {
     attribution: osmAttributes,
   }).addTo(map);
