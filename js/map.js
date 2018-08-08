6de1f46cf3026faa5eb74caa3f8402a0d9ad72ce
Artem Dadychenko <a.dadychenko@mail.freebee.by>
2018-08-08 23:35:24 +0300

    Add wifis request Adapt popup style

--- js/map.js
+++ js/map.js
@@ -6,30 +6,71 @@ function initMap() {
   var osmAttributes = 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
 
   var map = L
-  .map(containerId, {
-    minZoom: 10,
-  })
-  .setView(location, zoom);
+    .map(containerId, {
+      minZoom: 10,
+    })
+    .setView(location, zoom);
   L.tileLayer(osmUrl, {
     attribution: osmAttributes,
   }).addTo(map);
 
-  var wifiIcon = L.icon({
-    iconUrl: 'image/wifi.png',
-    iconSize: [23, 37],
-  })
+  getWifi(getWifiSuccess(map), getWifiError);
+}
 
-  var wifi = JSON.parse(wifiData);
-  for (var i = 0; i < wifi.length; i++) {
-    if (wifi[i].location) {
-      var marker = L.marker(
-        wifi[i].location.reverse(),
-        { icon: wifiIcon }).addTo(map);
+function getWifi(resolve, reject) {
+  fetch(`${api}/wifis`, {})
+    .then(res => {
+      if (res.status !== 200) {
+        throw new Error("Ошибка запроса");
+      }
 
-      marker.bindPopup(
-        `<h3 class="freebie-title">Wifi</h3>
-        ${wifi[i].address}`
-      );
+      return res.json();
+    })
+    .then(wifi => {
+      resolve(wifi);
+    })
+    .catch(err => {
+      console.error(err)
+      reject(err);
+    });
+}
+
+function getWifiSuccess(map) {
+  return function (wifis) {
+    const wifiIcon = L.icon({
+      iconUrl: 'image/wifi.png',
+      iconSize: [23, 37],
+    });
+
+    for (let i = 0; i < wifis.length; i++) {
+      if (wifis[i].location) {
+        const marker = L.marker(
+          wifis[i].location,
+          { icon: wifiIcon }).addTo(map);
+
+        marker.bindPopup(
+          `
+          <div class="freebee-popup-content">
+            <span class="freebee-point">Название: </span>
+            <span class="freebee-title">${wifis[i].title}</span>
+            <br><br>
+            <span class="freebee-point">Адрес: </span>
+            <span class="freebee-address"> ${wifis[i].address}</span>
+            <br><br>
+            <span class="freebee-point">Описание: </span>
+            <span class="freebee-description"> ${wifis[i].description}</span>
+          </div>
+          `
+        );
+      }
     }
   }
+}
+
+function getWifiError(err) {
+  M.toast({
+    html: 'Ошибка при получении данных',
+    displayLength: 1800,
+    classes: 'red lighten-1',
+  });
 }
