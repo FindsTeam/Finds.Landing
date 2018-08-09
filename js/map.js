0d413013ca9611863d8ab2289b0770074abfebef
Artem Dadychenko <a.dadychenko@mail.freebee.by>
2018-08-09 18:35:57 +0300

    Implement toilets request

--- js/map.js
+++ js/map.js
@@ -14,9 +14,75 @@ function initMap() {
     attribution: osmAttributes,
   }).addTo(map);
 
-  getWifi(getWifiSuccess(map), getWifiError);
+  getWifi(
+    getMarkersSuccess(map, 'image/wifi.png', getWifiPopupLayoutAsString),
+    getMarkersError
+  );
+
+  getToilets(
+    getMarkersSuccess(map, 'image/toilet.png', getToiletPopupLayoutAsString),
+    getMarkersError
+  );
 }
 
+// General data handlers
+function getMarkersSuccess(map, iconPath, getLayout) {
+  return function (wifis) {
+    const icon = L.icon({
+      iconUrl: iconPath,
+      iconSize: [25, 37],
+    });
+
+    for (let i = 0; i < wifis.length; i++) {
+      if (wifis[i].location) {
+        const marker = L.marker(
+          wifis[i].location,
+          { icon: icon }).addTo(map);
+
+        marker.bindPopup(getLayout(wifis[i]));
+      }
+    }
+  }
+}
+function getMarkersError(err) {
+  M.toast({
+    html: 'Ошибка загрузки данных',
+    displayLength: 1800,
+    classes: 'red lighten-1',
+  });
+}
+
+// toilets markers request
+function getToilets(resolve, reject) {
+  fetch(`${api}/toilets`, {})
+    .then(res => {
+      if (res.status !== 200) {
+        throw new Error("Ошибка запроса");
+      }
+
+      return res.json();
+    })
+    .then(toilets => resolve(toilets))
+    .catch(err => {
+      console.error(err);
+      reject(err);
+    });
+}
+function getToiletPopupLayoutAsString(toilet) {
+  return `
+  <div class="freebee-popup-content">
+    <span class="freebee-title">${toilet.title}</span>
+    <br><br>
+    <span class="freebee-point">Адрес: </span>
+    <span class="freebee-address"> ${toilet.address}</span>
+    <br><br>
+    <span class="freebee-point">Описание: </span>
+    <span class="freebee-description"> ${toilet.description || 'Отсутствует'}</span>
+  </div>
+  `
+}
+
+// wifis markers request
 function getWifi(resolve, reject) {
   fetch(`${api}/wifis`, {})
     .then(res => {
@@ -26,51 +92,23 @@ function getWifi(resolve, reject) {
 
       return res.json();
     })
-    .then(wifi => {
-      resolve(wifi);
-    })
+    .then(wifi => resolve(wifi))
     .catch(err => {
       console.error(err)
       reject(err);
     });
 }
-
-function getWifiSuccess(map) {
-  return function (wifis) {
-    const wifiIcon = L.icon({
-      iconUrl: 'image/wifi.png',
-      iconSize: [23, 37],
-    });
-
-    for (let i = 0; i < wifis.length; i++) {
-      if (wifis[i].location) {
-        const marker = L.marker(
-          wifis[i].location,
-          { icon: wifiIcon }).addTo(map);
-
-        marker.bindPopup(
-          `
-          <div class="freebee-popup-content">
-            <span class="freebee-point">Название: </span>
-            <span class="freebee-title">${wifis[i].title}</span>
-            <br><br>
-            <span class="freebee-point">Адрес: </span>
-            <span class="freebee-address"> ${wifis[i].address}</span>
-            <br><br>
-            <span class="freebee-point">Описание: </span>
-            <span class="freebee-description"> ${wifis[i].description}</span>
-          </div>
-          `
-        );
-      }
-    }
-  }
-}
-
-function getWifiError(err) {
-  M.toast({
-    html: 'Ошибка при получении данных',
-    displayLength: 1800,
-    classes: 'red lighten-1',
-  });
+function getWifiPopupLayoutAsString(wifi) {
+  return `
+  <div class="freebee-popup-content">
+    <span class="freebee-point">Название: </span>
+    <span class="freebee-title">${wifi.title}</span>
+    <br><br>
+    <span class="freebee-point">Адрес: </span>
+    <span class="freebee-address"> ${wifi.address}</span>
+    <br><br>
+    <span class="freebee-point">Описание: </span>
+    <span class="freebee-description"> ${wifi.description}</span>
+  </div>
+  `
 }
