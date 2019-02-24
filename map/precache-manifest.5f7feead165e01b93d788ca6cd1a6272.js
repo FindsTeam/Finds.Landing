d5d00c52517de0b4322049654a8f55affb154e48
GitStearis <egor.puisha@gmail.com>
2019-02-24 22:56:16 +0300

    Multiline input, new marker move logic, OSM zoom fix

---
+++ map/precache-manifest.5f7feead165e01b93d788ca6cd1a6272.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e30872d27b4ecb8b3f0",
+    "url": "./static/css/main.0523c18c.chunk.css"
+  },
+  {
+    "revision": "7e30872d27b4ecb8b3f0",
+    "url": "./static/js/main.7e30872d.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "be62d53dda61b6dd225a",
+    "url": "./static/css/2.ba74da39.chunk.css"
+  },
+  {
+    "revision": "be62d53dda61b6dd225a",
+    "url": "./static/js/2.be62d53d.chunk.js"
+  },
+  {
+    "revision": "a5113c91275c6c73785ff715f8ec93b6",
+    "url": "./static/media/wifi.a5113c91.svg"
+  },
+  {
+    "revision": "26830febd9b8d6a72d733a82d19db65a",
+    "url": "./static/media/toilet.26830feb.svg"
+  },
+  {
+    "revision": "c45c2ef8bcafc70d1211b63c398ee9d0",
+    "url": "./static/media/user.c45c2ef8.svg"
+  },
+  {
+    "revision": "76edbd6fc6ac5102a8361d27179a71f8",
+    "url": "./static/media/empty-landmark.76edbd6f.svg"
+  },
+  {
+    "revision": "64f0c21c9c8ac50a6f712b1e35fc4f7e",
+    "url": "./index.html"
+  }
+];
