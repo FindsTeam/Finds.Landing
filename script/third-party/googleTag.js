cf39c01ddec53b94b422b3151474ff054adbd60b
ArtemDadychenko <artem_dadychenko@mail.ru>
2020-09-26 22:59:17 +0300

    Extract third-party scripts to separate files

---
+++ script/third-party/googleTag.js
@@ -0,0 +1,5 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag("js", new Date());
+
+gtag('config', 'UA-122766115-1', { 'optimize_id': 'GTM-59ZBHKC'});
